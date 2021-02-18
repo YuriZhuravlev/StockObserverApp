b1f815df7481ba9ca2f8167d5b5b585ccd5176ef
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-02-18 09:44:48 +0300

    init commit

---
+++ app/src/main/java/com/zhuravlev/stockobserverapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhuravlev.stockobserverapp
+
+import android.os.Bundle
+import moxy.MvpAppCompatActivity
+
+class MainActivity : MvpAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

77120d5b487d4d7165367e07cedde0cac74b7d7c
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-03-03 10:57:59 +0300

    Инициализация вебсокета

--- app/src/main/java/com/zhuravlev/stockobserverapp/storage/net/ApiUtils.kt
+++ app/src/main/java/com/zhuravlev/stockobserverapp/storage/net/ApiUtils.kt
@@ -2,6 +2,8 @@ package com.zhuravlev.stockobserverapp.storage.net
 
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,6 +13,8 @@ const val TOKEN = "c0nukbv48v6qah6ron2g"
 
 private var okHttpClient = OkHttpClient()
 private var retrofit: Retrofit? = null
+private val request: Request = Request.Builder().build()
+private var webSocket: WebSocket? = null
 private var gson = Gson()
 private var api: FinnhubApi? = null
 
@@ -18,13 +22,20 @@ fun getOkHttpClient(): OkHttpClient {
     return okHttpClient
 }
 
+fun getWebSocket(): WebSocket {
+    if (webSocket == null) {
+        webSocket = okHttpClient.newWebSocket(request, WebSocketFinnhubListener())
+    }
+    return webSocket!!
+}
+
 fun getRetrofit(): Retrofit {
     if (retrofit == null) {
         retrofit = Retrofit.Builder()
-                .baseUrl(SERVER_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .build()
+            .baseUrl(SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
     }
     return retrofit!!
 }
---
+++ app/src/main/java/com/zhuravlev/stockobserverapp/storage/net/WebSocketFinnhubListener.kt
@@ -0,0 +1,33 @@
+package com.zhuravlev.stockobserverapp.storage.net
+
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
+
+
+class WebSocketFinnhubListener : WebSocketListener() {
+    override fun onOpen(webSocket: WebSocket, response: Response) {
+        super.onOpen(webSocket, response)
+    }
+
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        super.onMessage(webSocket, text)
+    }
+
+    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+        super.onMessage(webSocket, bytes)
+    }
+
+    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+        super.onClosing(webSocket, code, reason)
+    }
+
+    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+        super.onClosed(webSocket, code, reason)
+    }
+
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        super.onFailure(webSocket, t, response)
+    }
+}
